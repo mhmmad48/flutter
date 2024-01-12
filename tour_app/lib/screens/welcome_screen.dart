0c6e07158faadd32ba0d0f2a9243a48fb09d2023
unknown <mhmmadyasin154@gmail.com>
2024-01-12 15:06:38 +0300

    add

---
+++ tour_app/lib/screens/welcome_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class welcomescreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+  return Container(
+    decoration: const BoxDecoration(
+      image: DecorationImage(
+        image: AssetImage("assets/images/Travel-Insurancex.png"),
+        fit: BoxFit.cover,
+        opacity: 0.7,
+
+      )
+    ),
+    child: Material(
+      color: Colors.transparent,
+      child: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 65,horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Marah Tour",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5,
+                ),
+              ),
+              SizedBox(height: 2,),
+              Text(
+                "Enjoy the world",
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.9),
+                  fontSize: 35,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 1.5
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                  style: TextStyle(
+                    color: Colors.black38.withOpacity(0.7),
+                    fontSize: 16,
+                    letterSpacing: 1.2,
+
+                  ),
+                  ),
+              SizedBox(height: 30),
+              InkWell(onTap: (){
+                Navigator.push(context,MaterialPageRoute(
+                    builder:(context)=>HomePage(),));
+              },
+              child: Ink(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(color: Colors.white,
+                borderRadius:BorderRadius.circular(12),
+                ),
+                child: Icon(Icons.arrow_forward_ios,
+                color: Colors.black54,
+                  size: 20,
+                ),
+              ),)
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+  }
+}
